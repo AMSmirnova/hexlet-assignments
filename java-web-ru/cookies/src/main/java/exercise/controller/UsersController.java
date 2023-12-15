607b0de2e26f5868a1a1fefd42ed67d1f9bc9831
@hexlet/cli <support@hexlet.io>
2023-12-15 10:12:17 +0100

    submit java-web-ru/cookies

---
+++ java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,47 @@
+package exercise.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+import exercise.model.User;
+import exercise.util.NamedRoutes;
+import java.util.Collections;
+import java.util.Objects;
+
+import exercise.repository.UserRepository;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.http.Context;
+
+
+public class UsersController {
+
+    public static void build(Context ctx) throws Exception {
+        ctx.render("users/build.jte");
+    }
+
+    // BEGIN
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var user = UserRepository.find(id)
+                .orElse(null);
+        if (user == null || !Objects.equals(ctx.cookie("registered"), user.getToken())) {
+            ctx.redirect(NamedRoutes.buildUserPath());
+        } else {
+            ctx.render("users/show.jte", Collections.singletonMap("user", user));
+        }
+    }
+
+    public static void create(Context ctx) {
+        var firstName = ctx.formParamAsClass("firstName", String.class).get();
+        var lastName = ctx.formParamAsClass("lastName", String.class).get();
+        var email = ctx.formParamAsClass("email", String.class).get();
+        var password = ctx.formParamAsClass("password", String.class).get();
+        var token = Security.generateToken();
+        ctx.cookie("registered", token);
+        var user = new User(firstName, lastName, email, password, token);
+
+        UserRepository.save(user);
+        ctx.redirect(NamedRoutes.userPath(user.getId()));
+    }
+
+    // END
+}
