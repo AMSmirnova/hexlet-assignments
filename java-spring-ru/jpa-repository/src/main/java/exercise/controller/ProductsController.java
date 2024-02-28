bfd34da3a251afe19715f4fb56b81fad87a24a8a
@hexlet/cli <support@hexlet.io>
2024-02-28 21:08:03 +0100

    submit java-spring-ru/jpa-repository

--- java-spring-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
@@ -23,24 +23,11 @@ public class ProductsController {
 
     // BEGIN
     @GetMapping(path = "")
-    public List<Product> index(@RequestParam(defaultValue = "0") int min, @RequestParam(defaultValue = "0") int max) {
-        var products = productRepository.findAll(Sort.by(Sort.Order.asc("price")));
-        if (min != 0 && max != 0) {
-            return products.stream()
-                    .filter(p -> p.getPrice() <= max && p.getPrice() >= min)
-                    .toList();
-        }
-        if (min == 0 && max != 0) {
-            return products.stream()
-                    .filter(p -> p.getPrice() <= max)
-                    .toList();
-        }
-        if (min != 0 && max == 0) {
-            return products.stream()
-                    .filter(p -> p.getPrice() >= min)
-                    .toList();
-        }
-        return products;
+    public List<Product> index(
+            @RequestParam(defaultValue = "0") int min,
+            @RequestParam(defaultValue = Integer.MAX_VALUE + "") int max) {
+        Sort sort = Sort.by(Sort.Order.asc("price"));
+        return productRepository.findByPriceBetween(min, max, sort);
     }
     // END
 
--- java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
+++ java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.Sort;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // BEGIN
-    List<Product> findByPriceBetween(Integer minPrice, Integer maxPrice);
-//    List<Product> findByPriceLessThanEqual(Integer price);
-//    List<Product> findByPriceMoreThanEqual(Integer price);
+    List<Product> findByPriceBetween(Integer min, Integer max, Sort sort);
     // END
 }
